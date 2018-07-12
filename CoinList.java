1c3b662f6b27512fb912be956ea1179ba532874d
staniszevskyy <expunge96@hotmail.com>
2018-07-12 23:22:21 +0200

    working buying algorithm

--- CoinList.java
+++ CoinList.java
@@ -5,15 +5,15 @@ public class CoinList {
 
     public CoinList() {
         coinList = new Coin[9];
-        coinList[0] = new Coin(1, 50);
-        coinList[1] = new Coin(2, 50);
-        coinList[2] = new Coin(5, 50);
-        coinList[3] = new Coin(10, 50);
+        coinList[8] = new Coin(1, 50);
+        coinList[7] = new Coin(2, 50);
+        coinList[6] = new Coin(5, 50);
+        coinList[5] = new Coin(10, 50);
         coinList[4] = new Coin(20, 50);
-        coinList[5] = new Coin(50, 50);
-        coinList[6] = new Coin(100, 50);
-        coinList[7] = new Coin(200, 50);
-        coinList[8] = new Coin(500, 50);
+        coinList[3] = new Coin(50, 50);
+        coinList[2] = new Coin(100, 50);
+        coinList[1] = new Coin(200, 50);
+        coinList[0] = new Coin(500, 50);
 
     }
 
@@ -22,7 +22,7 @@ public class CoinList {
         for (Coin c : coinList){
             sum+=c.getVal()*c.getQuant();
         }
-        return sum/100;
+        return sum;
     }
 
 }
--- Main.java
+++ Main.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
 
+import static java.lang.Math.*;
+
 class menu {
     public static void mainMenu() {
         System.out.println("*************************************");
         System.out.println("Siemaneczko! Powiedz co chcesz zrobić:");
         System.out.println("1. Wplacic kase");
         System.out.println("2. Kupic napoj");
+        System.out.println("3. Sprawdz saldo");
         System.out.println("0. Zakoncz program");
         System.out.println("*************************************");
     }
@@ -26,6 +29,7 @@ class menu {
 
 }
 
+
 public class Main {
 
     public static void main(String args[]) {
@@ -35,58 +39,105 @@ public class Main {
         CoinList coinList = new CoinList();
         DrinkList drinkList = new DrinkList();
         Scanner input = new Scanner(System.in);
-        menu.mainMenu();
-        int ans = input.nextInt();
-        if (ans == 1) {
-            menu.insertCoinMenu();
-            //DO ZROBIENIA: gdy nauczysz sie wyjatkow
-            ans = input.nextInt();
-            while (ans != 0) {
-                for (Coin coin : userCoins.coinList) {
-                    if (ans == coin.getVal()) {
-                        coin.setQuant(coin.getQuant() + 1);
-                    }
-                }
-                System.out.println("Moneta wrzucona");
-                ans = input.nextInt();
-            }
 
-        } else if (ans == 2) {
-            menu.buyDrinkMenu();
-            ans = input.nextInt();
-            if (ans == 2) {
-                //WYPISZ NAPOJE
-                for (Drink drink : drinkList.drinks) {
-                    System.out.println(drink.getNum() + ". " + drink.getName());
+        while (true) {
+            menu.mainMenu();
+            int ans = input.nextInt();
+            if (ans == 1) {
+                menu.insertCoinMenu();
+                //DO ZROBIENIA: gdy nauczysz sie wyjatkow
+                ans = input.nextInt();
+                while (ans != 0) {
+                    for (Coin coin : userCoins.coinList) {
+                        if (ans == coin.getVal()) {
+                            coin.setQuant(coin.getQuant() + 1);
+                        }
+                    }
+                    System.out.println("Moneta wrzucona");
+                    System.out.println("Obecny hajs: " + userCoins.calcSum() / 100);
+                    ans = input.nextInt();
                 }
-            } else if (ans == 1) {
+
+            } else if (ans == 2) {
+                menu.buyDrinkMenu();
+                ans = input.nextInt();
+                if (ans == 2) {
+                    //WYPISZ NAPOJE
+                    for (Drink drink : drinkList.drinks) {
+                        System.out.println(drink.getNum() + ". " + drink.getName());
+                    }
+
+                }
                 //PODAJ NUMER NAPOJU
                 System.out.println("Podaj numer napoju ktory chcesz kupic");
                 ans = input.nextInt();
-                while (ans<30 || ans>49){
+                while (ans < 30 || ans > 49) {
                     System.out.println("Wybrano niepoprawny numer napoju");
                     ans = input.nextInt();
                 }
-                Drink drink=null;
-                for  (int i=0; i<drinkList.drinks.length; i++){
+                Drink drink = null;
+                for (int i = 0; i < drinkList.drinks.length; i++) {
                     if (drinkList.drinks[i].getNum() == ans)
                         drink = drinkList.drinks[i];
                 }
 
-                if (drink.getPrice() > userCoins.calcSum()){
+                if (drink.getPrice() > userCoins.calcSum()) {
                     System.out.println("Za malo hajsu biedaku");
                     //narazie koniec programu
                     return;
+                } else if (drink.getPrice() == userCoins.calcSum()) {
+                    zeroUserMoney(userCoins, coinList);
+                    System.out.println("Otrzymujesz napoj " + drink.getName() + ". Brak reszty. Smacznego");
+                } else if (ans == 2) {
+                    float rest = abs(drink.getPrice() - userCoins.calcSum());
+                    zeroUserMoney(userCoins, coinList);
+                    for (Coin c : coinList.coinList) {
+                        while (c.getQuant() != 0 && rest - c.getVal() >= 0) {
+
+                            rest -= c.getVal();
+                            Coin usermoney = findCoin(userCoins, c.getVal());
+                            usermoney.setQuant(usermoney.getQuant() + 1);
+                            c.setQuant(c.getQuant() - 1);
+
+                        }
+                    }
+                    System.out.println("Otrzymujesz napoj " + drink.getName() + ". Twoja reszta to: " + userCoins.calcSum() / 100);
                 }
 
+
+            } else {
+                System.out.println("Twoj hajs to: " + userCoins.calcSum() / 100);
             }
 
         }
 
-        for (Coin coin : userCoins.coinList) {
-            System.out.printf("%d groszy w ilosci: %d\n", coin.getVal(), coin.getQuant());
+
+    }
+
+    private static void zeroUserMoney(UserCoins userCoins, CoinList coinList) {
+        for (Coin uCoin : userCoins.coinList) {
+            int quant = uCoin.getQuant();
+            for (Coin bankCoin : coinList.coinList) {
+                if (bankCoin.getVal() == uCoin.getVal()) {
+                    uCoin.setQuant(0);
+                    bankCoin.setQuant(bankCoin.getQuant() + quant);
+                }
+            }
         }
     }
 
+    private static void dispCoins(CoinList userCoins) {
+        for (Coin coin : userCoins.coinList) {
+            System.out.printf("%d groszy w ilosci: %d\n", coin.getVal(), coin.getQuant());
 
+        }
+    }
+
+    private static Coin findCoin(CoinList coins, int val) {
+        for (Coin coin : coins.coinList) {
+            if (coin.getVal() == val)
+                return coin;
+        }
+        return null;
+    }
 }
