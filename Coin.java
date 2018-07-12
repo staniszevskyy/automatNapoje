933d154136664d7f7de39cd68635375e76c2e6ca
staniszevskyy <expunge96@hotmail.com>
2018-07-12 19:15:19 +0200

    Initial Commit

---
+++ Coin.java
@@ -0,0 +1,17 @@
+public class Coin {
+    private int val;
+    private int quant;
+
+    public Coin(int val, int quant) {
+        this.val = val;
+        this.quant = quant;
+    }
+
+    public void setQuant(int quant){
+        this.quant=quant;
+
+    }
+    public void setVal (int val){
+        this.val = val;
+    }
+}
---
+++ Drink.java
@@ -0,0 +1,11 @@
+public class Drink {
+    private String name;
+    private int quant;
+    private int price;
+
+    public Drink(String name, int quant, int price) {
+        this.name = name;
+        this.quant = quant;
+        this.price = price;
+    }
+}
